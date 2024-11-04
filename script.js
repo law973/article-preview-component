29ac935992ace52ae184cb2ecb846c3ab26ff8e9
law973 <lawright379@gmail.com>
2024-11-03 21:21:15 -0400

    added event listener for button

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const shareButton = document.getElementById('share_button');
+
+const toggleShareButton = () => {
+    shareButton.classList.toggle('active');
+};
+
+const toggleShareContainer = () => {
+    const shareContainer = document.getElementById('share_container');
+    shareContainer.classList.toggle('hidden');
+}
+
+const toggleShareOptions = () => {
+    toggleShareButton();
+    toggleShareContainer();
+};
+
+// Event listener
+shareButton.addEventListener('click', () => toggleShareOptions());
