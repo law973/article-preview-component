aa6abaa447981fc175d8e0cefb3d3c56f1b69204
Levi Wright <lawright379@gmail.com>
2024-11-04 21:05:08 -0400

    continuing to work on share container positioning

--- script.js
+++ script.js
@@ -9,9 +9,15 @@ const toggleShareContainer = () => {
     shareContainer.classList.toggle('hidden');
 }
 
+const togglePublicationContainer = () => {
+    const publicationContainer = document.getElementById('publication_container');
+    publicationContainer.classList.toggle('hidden');
+}
+
 const toggleShareOptions = () => {
     toggleShareButton();
     toggleShareContainer();
+    togglePublicationContainer();
 };
 
 // Event listener
